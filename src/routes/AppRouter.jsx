b9fe902631a3fa12dae0da3d7190eaf96fa2ed3d
teemo007 <lawtszkit@outlook.com>
2024-03-31 05:26:30 -0400

    Add GitHub Actions workflow for automatic deployment

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,24 +4,27 @@ import Home from "../pages/home/Home";
 import Search from "../pages/search/Search";
 import Categories from "../pages/categories/Categories";
 import { Outlet } from "react-router-dom";
-import ErrorPage from "../components/notfound/ErrorPage"
+import ErrorPage from "../components/notfound/ErrorPage";
 
-const router = createBrowserRouter([
-  {
-    element: (
-      <>
-        <Header />
-        <Outlet />
-      </>
-    ),
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/search", element: <Search /> },
-      { path: "/categories", element: <Categories /> },
-    ],
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ),
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/search", element: <Search /> },
+        { path: "/categories", element: <Categories /> },
+      ],
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 const AppRouter = () => {
   return <RouterProvider router={router} />;
